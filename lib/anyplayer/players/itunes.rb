28cc0d355e8357475a7754ff6dfef6858d563e4a
Sunny Ripert <sunny@sunfox.org>
2011-06-14 16:46:42 +0200

    Using rb-appscript to make iTunes faster

--- lib/anyplayer/players/itunes.rb
+++ lib/anyplayer/players/itunes.rb
@@ -1,56 +1,57 @@
+if RUBY_PLATFORM =~ /darwin/
+  require 'appscript'
+  include Appscript
+end
+
 module Anyplayer
   class Itunes < Player
     def playpause
-      tell_to 'playpause'
+      @app.playpause
     end
 
     def prev
-      tell_to 'previous track'
+      @app.previous_track
       super
     end
 
     def next
-      tell_to 'next track'
+      @app.next_track
       super
     end
 
     def voldown
-      tell_to 'set sound volume to sound volume - 10'
+      @app.sound_volume.set(volume + 10)
     end
 
     def volup
-      tell_to 'set sound volume to sound volume + 10'
+      @app.sound_volume.set(volume + 10)
     end
 
     def volume
-      tell_to 'return sound volume'
+      @app.sound_volume.get
     end
 
     def track
-      tell_to 'return name of current track'
+      @app.current_track.name.get
     end
 
     def artist
-      tell_to 'return artist of current track'
+      @app.current_track.artist.get
     end
 
     def album
-      tell_to 'return album of current track'
+      @app.current_track.album.get
     end
 
     def launched?
-      return false if RUBY_PLATFORM !~ /darwin/
-      nb = %x(osascript -e 'tell app "System Events" to count (every process whose name is "iTunes")' 2>/dev/null).rstrip
-      nb.match(/^\d+/) and nb.to_i > 0 ? true : false
+      return false unless RUBY_PLATFORM =~ /darwin/
+      return false unless app('System Events').processes.name.get.include? "iTunes"
+      @app = app('iTunes')
+      true
     end
 
     def name
       "iTunes"
-    end
-
-    private
-    def tell_to(command)
-      %x(osascript -e 'tell app "iTunes" to #{command}').rstrip
     end
   end
 end
