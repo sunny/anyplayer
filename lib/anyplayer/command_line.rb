68a5844d7a7e50f7ec1770786a09a2c5b546620a
Sunny Ripert <sunny@sunfox.org>
2013-05-15 16:33:44 +0200

    Removed deprecation test, fix indentation in command line

--- lib/anyplayer/command_line.rb
+++ lib/anyplayer/command_line.rb
@@ -20,33 +20,32 @@ module Anyplayer::CommandLine
 
     # Call it
     case argv.first
-      when "playpause" then player.playpause
-      when "play"      then player.play
-      when "pause"     then player.pause
-      when "next"      then player.next
-      when "prev"      then player.next
-      when "voldown"   then player.voldown
-      when "volup"     then player.volup
-      when "volume"    then puts player.volume
-      when "track"     then puts player.track
-      when "artist"    then puts player.artist
-      when "album"     then puts player.album
-      when "vote"      then player.vote
-      when "name"      then puts player.name
-      when "launched"  then exit(player.launched? ? 0 : 1)
-      when "playing"   then exit(player.playing? ? 0 : 1)
-      when "paused"    then exit(player.playing? ? 0 : 1)
-      else
-        puts <<USAGE
-    Usage: #{$0} [-v] [command]
+    when "playpause" then player.playpause
+    when "play"      then player.play
+    when "pause"     then player.pause
+    when "next"      then player.next
+    when "prev"      then player.next
+    when "voldown"   then player.voldown
+    when "volup"     then player.volup
+    when "volume"    then puts player.volume
+    when "track"     then puts player.track
+    when "artist"    then puts player.artist
+    when "album"     then puts player.album
+    when "vote"      then player.vote
+    when "name"      then puts player.name
+    when "launched"  then exit(player.launched? ? 0 : 1)
+    when "playing"   then exit(player.playing? ? 0 : 1)
+    when "paused"    then exit(player.playing? ? 0 : 1)
+    else
+      puts <<USAGE
+Usage: #{$0} [-v] [command]
 
-    Where command is one of: playpause, play, pause, next, prev, voldown, volup,
-    volume, track, artist, album, vote, name, launched.
+Where command is one of: playpause, play, pause, next, prev, voldown, volup,
+volume, track, artist, album, vote, name, launched.
 
-    Player connected: #{player.name}.
+Player connected: #{player.name}.
 USAGE
-
-        exit(1)
+      exit(1)
     end
 
   end
--- test/anyplayer_test.rb
+++ test/anyplayer_test.rb
@@ -1,14 +1,7 @@
 require "test_helper"
 
 class AnyplayerTest < MiniTest::Unit::TestCase
-  def test_deprecated_launched
-    assert_output nil, "Anyplayer.launched is deprecated: please initialize a new Selector and call player\n" do
-      deprecated_player = Anyplayer::launched
-      assert_kind_of Player, deprecated_player
-    end
-  end
-
-  # def test_loading_all_players
+  # def test_loading_all_players_does_not_explode
   #   PLAYERS.each do |player|
   #     require "anyplayer/players/#{player}"
   #   end
