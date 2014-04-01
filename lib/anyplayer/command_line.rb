af122f502fde6bf611e4cf40ccb93d34e6b2954a
Sunny Ripert <sunny@sunfox.org>
2014-04-01 16:00:58 +0200

    Reduce duplicity and complexity in command line

--- lib/anyplayer/command_line.rb
+++ lib/anyplayer/command_line.rb
@@ -4,6 +4,10 @@ require "anyplayer"
 module Anyplayer::CommandLine
   extend self
 
+  ACTIONS = %w(playpause play pause next prev voldown volup vote)
+  INFOS = %w(volume track artist album name)
+  BOOLEANS = %w(launched playing paused)
+
   def parse(argv)
     # Create a selector
     selector = Anyplayer::Selector.new
@@ -26,23 +30,12 @@ module Anyplayer::CommandLine
   private
 
   def command(player, command)
-    case command
-    when "playpause" then player.playpause
-    when "play"      then player.play
-    when "pause"     then player.pause
-    when "next"      then player.next
-    when "prev"      then player.next
-    when "voldown"   then player.voldown
-    when "volup"     then player.volup
-    when "volume"    then puts player.volume
-    when "track"     then puts player.track
-    when "artist"    then puts player.artist
-    when "album"     then puts player.album
-    when "vote"      then player.vote
-    when "name"      then puts player.name
-    when "launched"  then exit(player.launched? ? 0 : 1)
-    when "playing"   then exit(player.playing? ? 0 : 1)
-    when "paused"    then exit(player.playing? ? 0 : 1)
+    if ACTIONS.include?(command)
+      player.send(command)
+    elsif INFOS.include?(command)
+      puts player.send(command)
+    elsif BOOLEANS .include?(command)
+      exit(player.send("#{command}?") ? 0 : 1)
     else
       puts <<USAGE
 Usage: anyplayer [-v] [command]
---
+++ test/command_line_test.rb
@@ -0,0 +1,125 @@
+require "test_helper"
+require "anyplayer/command_line"
+
+class CommandLineTest < Minitest::Test
+  include FlexMock::TestCase
+
+  def setup
+    @player = flexmock()
+    @selector = flexmock()
+    @selector.should_receive(:player).and_return(@player)
+    flexmock(Anyplayer::Selector).should_receive(:new).and_return(@selector)
+  end
+
+
+  # ACTIONS
+
+  def test_playpause
+    flexmock(@player).should_receive(:playpause).once
+    Anyplayer::CommandLine.parse(%w(playpause))
+  end
+
+  def test_play
+    flexmock(@player).should_receive(:play).once
+    Anyplayer::CommandLine.parse(%w(play))
+  end
+
+  def test_pause
+    flexmock(@player).should_receive(:pause).once
+    Anyplayer::CommandLine.parse(%w(pause))
+  end
+
+  def test_next
+    flexmock(@player).should_receive(:next).once
+    Anyplayer::CommandLine.parse(%w(next))
+  end
+
+  def test_prev
+    flexmock(@player).should_receive(:prev).once
+    Anyplayer::CommandLine.parse(%w(prev))
+  end
+
+  def test_voldown
+    flexmock(@player).should_receive(:voldown).once
+    Anyplayer::CommandLine.parse(%w(voldown))
+  end
+
+  def test_volup
+    flexmock(@player).should_receive(:volup).once
+    Anyplayer::CommandLine.parse(%w(volup))
+  end
+
+  def test_vote
+    flexmock(@player).should_receive(:vote).once
+    Anyplayer::CommandLine.parse(%w(vote))
+  end
+
+
+  # INFOS
+
+  def test_volume
+    flexmock(@player).should_receive(:volume).once.and_return(42)
+    flexmock($stdout).should_receive(:puts).once.with(42)
+    Anyplayer::CommandLine.parse(%w(volume))
+  end
+
+  def test_track
+    flexmock(@player).should_receive(:track).once.and_return("Foo")
+    flexmock($stdout).should_receive(:puts).once.with("Foo")
+    Anyplayer::CommandLine.parse(%w(track))
+  end
+
+  def test_artist
+    flexmock(@player).should_receive(:artist).once.and_return("Foo")
+    flexmock($stdout).should_receive(:puts).once.with("Foo")
+    Anyplayer::CommandLine.parse(%w(artist))
+  end
+
+  def test_album
+    flexmock(@player).should_receive(:album).once.and_return("Foo")
+    flexmock($stdout).should_receive(:puts).once.with("Foo")
+    Anyplayer::CommandLine.parse(%w(album))
+  end
+
+  def test_name
+    flexmock(@player).should_receive(:name).once.and_return("Foo")
+    flexmock($stdout).should_receive(:puts).once.with("Foo")
+    Anyplayer::CommandLine.parse(%w(name))
+  end
+
+
+  # BOOLEANS
+
+  def test_launched_true
+    flexmock(Anyplayer::CommandLine).should_receive(:exit).once#.with(1)
+    flexmock(@player).should_receive(:launched?).once.and_return(true)
+    Anyplayer::CommandLine.parse(%w(launched))
+  end
+  def test_launched_false
+    flexmock(Anyplayer::CommandLine).should_receive(:exit).once#.with(0)
+    flexmock(@player).should_receive(:launched?).once.and_return(false)
+    Anyplayer::CommandLine.parse(%w(launched))
+  end
+
+  def test_playing_true
+    flexmock(Anyplayer::CommandLine).should_receive(:exit).once#.with(1)
+    flexmock(@player).should_receive(:playing?).once.and_return(true)
+    Anyplayer::CommandLine.parse(%w(playing))
+  end
+  def test_playing_false
+    flexmock(Anyplayer::CommandLine).should_receive(:exit).once#.with(0)
+    flexmock(@player).should_receive(:playing?).once.and_return(false)
+    Anyplayer::CommandLine.parse(%w(playing))
+  end
+
+  def test_paused_true
+    flexmock(Anyplayer::CommandLine).should_receive(:exit).once#.with(1)
+    flexmock(@player).should_receive(:paused?).once.and_return(true)
+    Anyplayer::CommandLine.parse(%w(paused))
+  end
+  def test_paused_false
+    flexmock(Anyplayer::CommandLine).should_receive(:exit).once#.with(0)
+    flexmock(@player).should_receive(:paused?).once.and_return(false)
+    Anyplayer::CommandLine.parse(%w(paused))
+  end
+end
