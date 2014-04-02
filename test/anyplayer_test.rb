132fa050fc853d5a29e17953b50aebbcb42293b9
Sunny Ripert <sunny@sunfox.org>
2014-04-02 16:07:20 +0200

    Anyplayer tests

--- test/anyplayer_test.rb
+++ test/anyplayer_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 class AnyplayerTest < Minitest::Test
-  # def test_loading_all_players_does_not_explode
-  #   PLAYERS.each do |player|
-  #     require "anyplayer/players/#{player}"
-  #   end
-  # end
+  def test_loading_all_players_does_not_explode
+    PLAYERS.each do |player|
+      require "anyplayer/players/#{player}"
+    end
+  rescue LoadError
+    # Allow LoadErrors that depend on Windows
+  end
+
+  def test_launched
+    assert_kind_of Anyplayer::Player, Anyplayer.launched
+  end
 end
