8175c7c8fbccb57eeddbb320cddc47d8e467aeec
Sunny Ripert <sunny@sunfox.org>
2014-07-07 16:02:44 +0200

    Reorder players to favor Windows

--- lib/anyplayer.rb
+++ lib/anyplayer.rb
@@ -1,13 +1,20 @@
 require "timeout"
 
 module Anyplayer
+  # Ordering of the players should put the most-used players and the fastest to
+  # test at the top.
+  #
+  # iTunes Windows has been placed at the top because of some linux false
+  # positives.
+  # https://github.com/sunny/anyplayer/issues/8
+
   PLAYERS = %w(
+    itunes_windows
     rhythmbox
     mpd
     xmms2
     amarok
     itunes_mac
-    itunes_windows
     spotify_mac
     rdio_mac
   )
