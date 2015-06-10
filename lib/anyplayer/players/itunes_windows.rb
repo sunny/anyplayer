1b3aaf98f8ac04dac8ad3a5c38cabd575efcb2f4
Sunny Ripert <sunny@sunfox.org>
2015-06-10 13:08:47 +0200

    Fix invalid multibyte char

--- lib/anyplayer/players/itunes_windows.rb
+++ lib/anyplayer/players/itunes_windows.rb
@@ -1,7 +1,7 @@
 begin
   require 'win32ole'
 rescue LoadError => e
-  raise LoadError.new("#{e.message} — You probably aren't on Windows")
+  raise LoadError.new("#{e.message} - You probably aren't on Windows")
 end
 
 class Anyplayer::ItunesWindows < Anyplayer::Player
